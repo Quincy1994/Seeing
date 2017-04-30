8c978cbfe62e0d53a6d0e682d8db2d0ab5b6f7ee
yinglish1997 <yinglish1997@foxmail.com>
2017-04-30 21:40:40 +0800

    path of dicts

---
+++ FocusAndEmotionAnalysis/pathConfig.java
@@ -0,0 +1,45 @@
+package EmotionAnalysis.myAnalysis;
+
+public class pathConfig {
+
+	/**
+	 * @param args
+	 */
+	  public static String positiveDictPath = "";
+	  public static String negativeDictPath= "";
+	
+	   public static String roleDict = "source/角色.txt";
+		public static String hero = "source/男主角.txt";
+		public static String heroine = "source/女主角.txt";
+		public static String  villain= "source/反派.txt";
+		public static String costar = "source/配角.txt";
+				
+		public static String manufactureDict = "source/制作.txt";
+		public static String scriptwriter = "source/编剧.txt";
+		public static String company = "source/出品公司.txt";
+		public static String director = "source/导演.txt";
+		public static String  scence= "source/选景.txt";
+		
+		public static String storyDict = "source/剧情.txt";
+		public static String start = "source/开头.txt";
+		public static String develop = "source/发展.txt";
+		public static String end = "source/结局.txt";
+		public static String  cryPoint = "source/笑点.txt";
+		public static String laughtPoint = "source/泪点.txt";
+		
+		public static String themeDict = "source/主题.txt";
+		public static String  style= "source/风格.txt";
+		public static String  themeContent = "source/题材内容.txt";
+		
+		public static String shitingDict = "source/视听.txt";
+		public static String action = "source/动作.txt";
+		public static String frame = "source/画面.txt";
+		public static String   cameraLen= "source/镜头.txt";
+		public static String music = "source/音乐.txt";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
